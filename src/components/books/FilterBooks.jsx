ce4f26f70fdcc925d7ab2bd7a007ccdef24109df
Tikhomirowww <you@example.com>
2022-10-31 13:57:48 +0600

    first commit

---
+++ src/components/books/FilterBooks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilterBooks = () => {
+  return (
+    <div>FilterBooks</div>
+  )
+}
+
+export default FilterBooks
---
+++ src/contexts/FavContextProvider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FavContextProvider = () => {
+  return (
+    <div>FavContextProvider</div>
+  )
+}
+
+export default FavContextProvider
