7d6339471151424248aced0dddf0a1bee22d3a72
Марат Апиев <maratapiev@MacBook-Air-Marat.local>
2022-11-03 11:35:50 +0600

    Added HomePage component

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -90,6 +90,41 @@ const HomePage = () => {
       title: 'Lake view',
       description: '4.74M views',
     },
+    {
+      src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
+      title: 'Night view',
+      description: '4.21M views',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
+      title: 'Lake view',
+      description: '4.74M views',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
+      title: 'Night view',
+      description: '4.21M views',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
+      title: 'Lake view',
+      description: '4.74M views',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36',
+      title: 'Mountain view',
+      description: '3.98M views',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1502657877623-f66bf489d236',
+      title: 'Night view',
+      description: '4.21M views',
+    },
+    {
+      src: 'https://images.unsplash.com/photo-1527549993586-dff825b37782',
+      title: 'Lake view',
+      description: '4.74M views',
+    },
   ];
   
 
@@ -130,6 +165,8 @@ const HomePage = () => {
     <div style={{display: 'flex', justifyContent: 'center', paddingTop: '30px'}}>
     <Box
       sx={{
+        background: '#f0c33b',
+        borderRadius: '20px',
         background: '#f0c33b',
         borderRadius: '20px',
         display: 'flex',
