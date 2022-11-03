b9e757f75fd1dc83282bd8307bb3da9969796eb1
Tikhomirowww <you@example.com>
2022-11-03 15:57:16 +0600

    add book

--- src/Pages/AddBookPage.jsx
+++ src/Pages/AddBookPage.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import AddBooks from '../components/books/AddBooks';
 
 const AddBookPage = () => {
   return (
-    <div>AddBookPage</div>
+    <div>
+        <AddBooks />
+    </div>
   )
 }
 
